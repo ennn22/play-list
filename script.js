e79eb597377686707d074d9dc2ebdee9e3f5f925
en <huien122@gmail.com>
2023-09-01 16:06:59 +0800

    Modified js for musicvideo content.

--- script.js
+++ script.js
@@ -28,30 +28,6 @@ const musicvideo = [
     img: "https://fiverr-res.cloudinary.com/videos/so_71.701272,t_main1,q_auto,f_auto/xeanzaum3r4omqbp5qru/create-youtube-channel-and-10-lofi-music-animation-videos-8200.png",
     name: "Stay See",
     duration: "2:02:50"
-  },
-  {
-    title: "Interstellar: A Trip Hop Mix",
-    img: "https://i.ytimg.com/vi/n61ULEU7CO0/maxresdefault.jpg",
-    name: "Confused Bi-Product of a Misinformed Culture",
-    duration: "1:15:55"
-  },
-  {
-    title: "Interstellar: A Trip Hop Mix",
-    img: "https://i.ytimg.com/vi/n61ULEU7CO0/maxresdefault.jpg",
-    name: "Confused Bi-Product of a Misinformed Culture",
-    duration: "1:15:55"
-  },
-  {
-    title: "Best of lofi hip hop 2021",
-    img: "https://static.vecteezy.com/system/resources/thumbnails/020/734/052/small_2x/animated-studying-lo-fi-background-late-night-homework-2d-cartoon-character-animation-with-nighttime-cozy-bedroom-interior-on-background-4k-footage-with-alpha-channel-for-lofi-music-aesthetic-video.jpg",
-    name: "S!X - Music",
-    duration: "2:02:50"
-  },
-  {
-    title: "Inspiring mix | lofi hip hop | Chillhop, Jazzhop, Chillout [Study/Sleep/Game]",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT05k3_OhVUWaY4ADjbnvLrIis1bWfv9L3-Wg&usqp=CAU",
-    name: "Dreamy",
-    duration: "1:00:07"
   }
 ]
 
@@ -75,5 +51,3 @@ const addToList = (musicvideo) => {
 };
 
 musicvideo.forEach(addToList);
-
-
